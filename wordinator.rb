9965d167dd19a83328716caf2e1f891a0a21e6b8
Scott Hale <shalemont@gmail.com>
2014-10-12 16:55:37 -0700

    rubocop it

--- wordinator.rb
+++ wordinator.rb
@@ -2,6 +2,22 @@ require 'sinatra'
 require 'sinatra/json'
 require 'shotgun'
 
+def one_length
+  (params[:wordone]).length
+end
+
+def one_sort
+  (params[:wordone]).chars.sort { |a, b| a.casecmp(b) } .join
+end
+
+def two_length
+  (params[:wordtwo]).length
+end
+
+def two_sort
+  (params[:wordtwo]).chars.sort { |a, b| a.casecmp(b) } .join
+end
+
 get '/' do
   'Welcome to Wordinator <br>
   Please type "/input_words" into browser after "localhost:9393", <br>
@@ -12,25 +28,8 @@ get '/input_words' do
   erb :input_words
 end
 
-
-def one_length
-    (params[:wordone]).length
-end
-
-def one_sort
-    (params[:wordone]).chars.sort { |a, b| a.casecmp(b) } .join
-end
-
-def two_length
-     (params[:wordtwo]).length
-end
-
-def two_sort
-    (params[:wordtwo]).chars.sort { |a, b| a.casecmp(b) } .join
-end
-
 post '/input_words' do
-    if one_length != two_length
+  if one_length != two_length
     json "#{params[:wordone]} and #{params[:wordtwo]} are NOT anagrams"
   elsif one_length == two_length && one_sort == two_sort
     json "#{params[:wordone]} and #{params[:wordtwo]} ARE anagrams!!!"
